00e11c8a04c99d5d650c5b236582c9053ead967b
hacksalot <hacksalot@indevious.com>
2018-02-01 05:13:49 -0500

    feat: introduce simple-custom-helper theme

---
+++ helpers/my-helper.js
@@ -0,0 +1,23 @@
+
+/**
+Test helper definitions for HackMyResume / FluentCV.
+@license MIT. See LICENSE.md for details.
+@module helpers/my-helper
+ */
+
+(function() {
+
+  // Block helper function definitions.
+
+  var MyTestHelpers = module.exports = {
+
+    /**
+    Return true if either value is truthy.
+    @method either
+    */
+    myHelper: function(val, options) {
+      return val.toUpperCase();
+    }
+  };
+
+}).call(this);
