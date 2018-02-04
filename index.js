99b73f2f2837e3d0ec24b221963f352059d7a946
hacksalot <hacksalot@indevious.com>
2018-02-04 03:12:50 -0500

    feat: mount new themes on module object

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ Mount individual themes onto the module object for convenience.
   module.exports = {
 
     themes: {
-      'simple-custom-helper':
-        require('./fresh/simple-custom-helper/theme.json'),
-      UPPERcase:
-        require('./fresh/UPPERcase/theme.json')
+      'simple-custom-helper': require('./fresh/simple-custom-helper/theme.json'),
+      'UPPERcase': require('./fresh/UPPERcase/theme.json'),
+      'relocated-base': require('./fresh/relocated-base/theme.json'),
+      'simplest-non-inherited': require('./fresh/simplest-non-inherited'),
+      'fresh-theme-hello-world': require('./node_modules/fresh-theme-hello-world'),
+      'fresh-theme-underscore': require('./node_modules/fresh-theme-underscore')
     }
 
   };
